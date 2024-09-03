0f2d613f3cf3c8d10a666a36d80fd2457e8c1217
Kevin KIN-FOO <kkinfoo@smartandconnective.com>
2024-09-03 20:29:40 +0200

    it's alive

---
+++ src/core/main.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppModule } from './app.module';
+
+@Module({ imports: [AppModule, ConfigModule.forRoot()] })
+export class MainModule {}
